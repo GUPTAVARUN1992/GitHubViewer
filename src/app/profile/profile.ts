c65b55a68b2d402216a70aa706fe9b2c2ad43f66
varun.gupta <varun.gupta@outlook.in>
2025-12-28 22:25:13 +0530

    Added button to navigate back to followers page

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,12 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 export class Profile implements OnInit {
   id!: number;
   username!: string;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = params['id'];
       this.username = params['username'];
       console.log('Hello, ' + this.username + ' with id ' + this.id);
+    });
+  }
+
+  backToFollowers() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
     });
   }
 }
