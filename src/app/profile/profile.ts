7d96ff2990832e5e3eadbf185b51ced818df0bbf
varun.gupta <varun.gupta@outlook.in>
2025-12-28 21:29:17 +0530

    Improved followers page

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
-
+export class Profile implements OnInit {
+  id!: number;
+  constructor(private route: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      console.log(this.id);
+    });
+  }
 }
